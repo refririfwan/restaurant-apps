b1c52c6ec86f492164d4177638f213badb456601
Refri Rifwan <refririfwan@gmail.com>
2021-06-01 10:26:03 +0700

    Menerapkan dasar responsibilitas tampilan pada perangkat seluler (Implementing Small Things).

--- webpack.common.js
+++ webpack.common.js
@@ -21,6 +21,12 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
     ],
   },
   plugins: [
